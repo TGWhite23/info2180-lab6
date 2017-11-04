1b7bee0d4b6ab9ec29d676314db84b51154438b7
Titania <titania.white@mymona.uwi.edu>
2017-11-04 00:20:11 +0000

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // JavaScript File
- function sayHello() {
-  alert("Hey There");
+ function searchButton() { 
+ var inpuText = document.getElementById('searchWord').value;  
                var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       alert(this.responseText);
+	  let myDiv = document.getElementById('result');
+		myDiv.innerHTML = this.responseText;
     }else
     {
      console.log(this)
     }
   };
-  xhttp.open("GET", "https://info2180-lab6-titaniagwhite.c9users.io/request.php?q=definition", true);
+  xhttp.open("GET", "request.php?q=" + inpuText, true);
   xhttp.send();
-    
+  
+  
+ 
  }
+ 
