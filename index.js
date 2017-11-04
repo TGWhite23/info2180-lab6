4de5ddf4ea0f6d418530a7890311ac3732d8bc7e
Titania <titania.white@mymona.uwi.edu>
2017-11-04 00:24:22 +0000

    third commit

--- index.js
+++ index.js
@@ -4,9 +4,8 @@
                var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      alert(this.responseText);
-	  let myDiv = document.getElementById('result');
-		myDiv.innerHTML = this.responseText;
+       let myDiv = document.getElementById('result');
+		myDiv.innerHTML = this.responseText;	  
     }else
     {
      console.log(this)
@@ -18,4 +17,54 @@
   
  
  }
+ 
+ function getAllButton() { 
+	
+               var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      
+	  let myDiv = document.getElementById('result');
+	let txt = this.responseText;
+	parser = new DOMParser();
+	xmlDoc = parser.parseFromString(txt,"text/xml");
+	var newList = document.createElement("ol");
+	
+	for(var i = 0; i < (xmlDoc.getElementsByTagName("definition")).length; i++)
+	{	var newListing = document.createElement("li");
+		var heading = document.createElement("h3");
+		var headingText = document.createTextNode(xmlDoc.getElementsByTagName("definition")[i].getAttribute("name"));		
+		heading.appendChild(headingText);
+		var paragraph1 = document.createElement("p");
+		var definitionText = document.createTextNode(xmlDoc.getElementsByTagName("definition")[i].childNodes[0].nodeValue);
+		paragraph1.appendChild(definitionText);
+		var paragraph2 = document.createElement("p");
+		var authorText = document.createTextNode("-" + xmlDoc.getElementsByTagName("definition")[i].getAttribute("author"));
+		paragraph2.appendChild(authorText);
+		
+		newListing.appendChild(heading);
+		newListing.appendChild(paragraph1);
+		newListing.appendChild(paragraph2);
+		newList.appendChild(newListing);
+	}
+	
+	
+		myDiv.appendChild(newList);	
+	
+	 
+    }else
+    {
+     console.log(this)
+    }
+  };
+  xhttp.open("GET","request.php?all=true"  , true);
+  xhttp.send();
+  
+  
+ 
+ }
+ 
+
+
+ 
  
--- request.php
+++ request.php
@@ -3,7 +3,10 @@
 // accept a term (keyword)
 // respond with a value
 
-$query = $_GET['q'];
+
+
+
+
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -14,5 +17,38 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+if (isset($_GET['q'])){
+	$query = $_GET['q'];
+	print "<h3>" . strtoupper($query) . "</h3>";
+	print "<p>" . $definition[$query] . "</p>";
+}
+
+if (isset ($_GET['all'])){
+	$txt = '<?xml version="1.0" encoding="UTF-8"?> 
+<entries> 
+    <definition name="definition" author="John"> 
+        a statement of the exact meaning of a word, especially in a 
+dictionary. 
+</definition> 
+    <definition name="bar" author="Mary"> 
+        a place that sells alcholic beverages 
+</definition> 
+    <definition name="ajax" author="Kimberly"> 
+        technique which involves the use of javascript and xml 
+</definition> 
+	<definition name = "html" author = "Bill">
+			The standard markup language for creating web pages and web applications.
+</definition>
+	<definition name="css" author="Jill">
+		A style sheet language used for describing the presentation of a document written in a markup language.
+</definition>
+	<definition name="javascript" author="Will">
+		A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+</definition>
+	<definition name="php" author="sam">
+		A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+</definition>	
+</entries>';
+	print $txt;
+}
+?>
